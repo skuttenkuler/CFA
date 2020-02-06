d8ee826d910c73cfe1af1d51c1e1cc19b39d358b
skuttenkuler <samkiddmusic@gmail.com>
2020-02-05 20:41:42 -0800

    server listening, schemas working

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const exphbs = require("express-handlebars")
 //set PORT
 const PORT = process.env.PORT || 3000;
 
-//ROUTES
-require("./routes/api-routes")(app)
-require("./routes/html-routes")(app)
-
+const db = require("./models");
 //set handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
@@ -17,9 +14,12 @@ app.set("view engine", "handlebars");
 //parser
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-//use routes
+
 //use static files
 app.use(express.static("public"));
+//ROUTES
+require('./routes/html-routes')(app)
+//require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
