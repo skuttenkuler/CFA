b3212ca0f6aee65243758cd2d9ad81c5bd1e9423
skuttenkuler <samkiddmusic@gmail.com>
2020-02-10 16:12:28 -0800

    new models, new config, auth

--- models/artist.js
+++ models/artist.js
@@ -1,31 +1,65 @@
 
-// Creating our Artist model
+//ARTIST MODEL
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
-module.exports = function(sequelize, DataTypes) {
-  var Artist = sequelize.define("Artist", {
-    type: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    
+const ArtistSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+// Execute before each user.save() call
+ArtistSchema.pre("save", function(callback) {
+  let artist = this;
+
+  // Break out if the password hasn't changed
+  if (!artist.isModified("password")) return callback();
+
+  // Password changed so we need to hash it
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return callback(err);
+
+    bcrypt.hash(artist.password, salt, null, function(err, hash) {
+      if (err) return callback(err);
+      artist.password = hash;
+      callback();
+    });
   });
+});
 
-
-  return Artist;
+ArtistSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
+
+const Artist = mongoose.model("Artist", ArtistSchema);
+
+module.exports = Artist;
--- server.js
+++ server.js
@@ -1,84 +1,27 @@
 //install packages
 const express = require("express");
+var cors = require("cors");
+var jwt = require('express-jwt');
+const routes = require('./routes');
 const app = express();
-const session = require('express-session')
-const passport = require('passport')
 const bodyParser = require('body-parser')
-var expressValidator = require('express-validator');
-const Store = require("express-mysql-session")(session);
-
-//set PORT
 const PORT = process.env.PORT || 3000;
+
+//parser
+app.use(cors());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//set PORT
 
 const db = require("./models");
 
-//sql session
-//production jaws db
-var sqlStore = new Store ({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "cfa_db"
-});
-
-
-//parser
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-app.use(expressValidator());
+app.use(routes)
 
 
 //use static files
 app.use(express.static("public"));
-//required for passport
-app.use(session({ store: sqlStore, 
-                  secret: "concerts for all",
-                  resave: true,
-                  saveUninitialized: true,
-                  cookie : {secure:false} }));
-app.use(passport.initialize());
-app.use(passport.session());//persistant logins
-app.use(bodyParser.urlencoded({ extended: false }));
-///
-// middleware to send user info to front end
-app.use((req, res, next) => {
-	
-	if (req.session && req.user.type === 'artist') {
-		db.Artist.findOne({
-			where: { id: req.user.id }
-		}).then((user, err) => {
-			if (err) {
-				console.log(err);
-      }
-      console.log(req.session.user)
-			req.user = user;
-			req.session.user = user;  //refresh the session value
-      res.locals.user = user;
-      
-			next();
-		});
-	} else if(req.session && req.user.type === 'user') {
-		db.User.findOne({
-			where: { id: req.user.id }
-		}).then((user, err) => {
-			if (err) {
-				console.log(err);
-      }
-      console.log(req.session.user)
-			req.user = user;
-			req.session.user = user;  //refresh the session value
-      res.locals.user = user;
-      
-			next();
-		});}else {
-		next();
-	}
-});
-//ROUTES
-require('./routes/html-routes')(app)
-require('./routes/api-routes.js')(app);
-require('./routes/passport-routes')(app)
+
 
 
 db.sequelize.sync().then(function() {
