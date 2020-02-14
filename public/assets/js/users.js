d56502cf64eccd4e988a98fa523b7b0bdfa5cb60
skuttenkuler <samkiddmusic@gmail.com>
2020-02-14 10:36:45 -0800

    complete

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -15,22 +15,30 @@ function getConcerts(){
     }).then(res => {
         
         for( var i = 0; i< res.length; i ++){
+            let name = res[i].artist;
+            let timeday = res[i].date;
+            let city = res[i].city;
+            let state = res[i].state;
+            let club = res[i].venue;
+        
+
            concertBlock = $("<li>")
             //console.log(res[i].artist)
-        ticket = $("<div>").addClass("ticket");
-        date = $("<div>").addClass("date");
-        dateData = $("<span>").addClass("datetime").text(res[i].date);
+        var ticket = $("<div>").addClass("ticket");
+        var date = $("<div>").addClass("date");
+        var dateData = $("<span>").addClass("datetime").text(timeday);
             date.append(dateData);
-        artist = $("<div>").addClass("artist");
-        artistData = $("<span>").addClass("name").text(res[i].artist);
+        var artist = $("<div>").addClass("artist");
+        var artistData = $("<span>").addClass("name").text(name);
             artist.append(artistData);
-        location = $("<div>").addClass("location");
-        cityState = $("<span>").text(res[i].city +", "+res[i].state);
-        br = $("</br>");
-        venue = $("<span>").addClass("venue").text(res[i].venue);
+        var location = $("<div>").addClass("location");
+        var cityState = $("<span>").text(city +", "+ state);
+        var br = $("</br>");
+        var venue = $("<span>").addClass("venue").text(club);
             location.append(cityState, br, venue )
         ticket.append(date,artist,location);
         concertBlock.append(ticket)
+        concerts.append(concertBlock    )
         }
      
 
